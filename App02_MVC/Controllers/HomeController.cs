edf5fbbdad6b2a45ecb42e6b0fdd43dd432db7f5
Md. Mahbubur Rahman <semahbub@gmail.com>
2020-04-03 19:59:30 +0600

    Initial commit of this project on April 03, 2020

---
+++ App02_MVC/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using App02_MVC.Models;
+
+namespace App02_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            //return View("Test");
+            return View("about2");
+        }
+
+        public IActionResult ShowFiles()
+        {
+            return View("Filelist");
+        }
+
+        public IActionResult Contact()
+        {
+            return View("ourpage");
+        }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ App02_MVC/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App02_MVC.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public string FullName()
+        {
+            return this.FirstName + " " + this.LastName;
+        }
+    }
+}
